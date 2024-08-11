982994c835f66dfd27d3e6c9cc3c0c99e07c0246
Andyr18 <andyroaluciano2019@gmail.com>
2024-08-11 06:09:35 -0400

    Update models.py

--- app/app/models.py
+++ app/app/models.py
@@ -1,1 +1,11 @@
+from datetime import datetime
+
+class Drug:
+    def __init__(self, name, quantity, expiry_date):
+        self.name = name
+        self.quantity = quantity
+        self.expiry_date = expiry_date
+
+    def is_expired(self):
+        return datetime.now().date() > self.expiry_date
 
